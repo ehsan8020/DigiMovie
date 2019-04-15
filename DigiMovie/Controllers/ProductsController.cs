3daeff4c28d501bdc194c3a3abe13a571ad8148c
Administrator <Administrator@TEACHER-202>
2019-04-15 20:34:49 +0430

    Delete-Create Product

--- DigiMovie/Controllers/ProductsController.cs
+++ DigiMovie/Controllers/ProductsController.cs
@@ -18,50 +18,6 @@ namespace DigiMovie.Controllers
 
         public IActionResult Index()
         {
-            #region TestCodes
-
-            //var car = new Product();
-            //car.Id = 1;
-            //car.Title = "Benz";
-            //car.Specification = "Best Vehicle In World";
-            //car.NumberInStock = 2;
-            //car.IsExists = true;
-
-            //return View(car);
-
-
-
-            //var p1 = new Product();
-            //p1.Id = 1;
-            //p1.Title = "Benz";
-            //p1.Specification = "Best Vehicle In World";
-            //p1.NumberInStock = 2;
-            //p1.IsExists = true;
-
-            //var p2 = new Product();
-            //p2.Id = 2;
-            //p2.Title = "BMW";
-            //p2.Specification = "Most Beautiful Vehicle In World";
-            //p2.NumberInStock = 1;
-            //p2.IsExists = true;
-
-            //var p3 = new Product();
-            //p3.Id = 3;
-            //p3.Title = "Pride";
-            //p3.Specification = "Most Dangerous Vehicle In World";
-            //p3.NumberInStock = 1000;
-            //p3.IsExists = true;
-
-
-            //var listOfProducts = new List<Product>();
-            //listOfProducts.Add(p1);
-            //listOfProducts.Add(p2);
-            //listOfProducts.Add(p3);
-
-
-            //return View(listOfProducts);
-
-            #endregion
             var list = _context.Products.ToList();
             return View(list);
         }
@@ -76,6 +32,62 @@ namespace DigiMovie.Controllers
                 return NotFound();
 
             return View(product);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteDone(int? id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Create(string Title, string IsExists, short NumberInStock, int Price, string Specification)
+        {
+            var product = new Product();
+            product.Title = Title;
+            product.NumberInStock = NumberInStock;
+
+            if(IsExists == "E")
+            {
+                product.IsExists = true;
+            }
+            else
+            {
+                product.IsExists = false;
+            }
+
+
+            product.Price = Price;
+            product.Specification = Specification;
+
+             _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
     }
---
+++ DigiMovie/Data/Migrations/20190415135059_AddTestDataToProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DigiMovie.Data.Migrations
+{
+    public partial class AddTestDataToProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO [dbo].[Products] (  [Title], [IsExists], [NumberInStock], [Specification], [Price]) VALUES ( N'محصول نمونه 1', 1, 1, N'توضیحات مربوط به محصول نمونه 1', 1000)
+            INSERT INTO[dbo].[Products](  [Title], [IsExists], [NumberInStock], [Specification], [Price]) VALUES(N'محصول نمونه 2', 1, 2, N'توضیحات مربوط به محصول نمونه 2', 2000)
+            INSERT INTO[dbo].[Products]
+             (  [Title], [IsExists], [NumberInStock], [Specification], [Price]) VALUES(N'محصول نمونه 3', 1, 3, N'توضیحات مربوط به محصول نمونه 3', 3000)
+            INSERT INTO[dbo].[Products]
+            (  [Title], [IsExists], [NumberInStock], [Specification], [Price]) VALUES(N'محصول نمونه 4', 0, 4, N'توضیحات مربوط به محصول نمونه 4', 4000)
+            INSERT INTO[dbo].[Products] (  [Title], [IsExists], [NumberInStock], [Specification], [Price]) VALUES(N'محصول نمونه 5', 1, 5, N'توضیحات مربوط به محصول نمونه 5', 5000)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [dbo].[Products]");
+        }
+    }
+}
