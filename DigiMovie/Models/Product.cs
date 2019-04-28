8039be60384f021de27ea4d7ac2f02a1516f0eb5
Ehsan <ehsan8020@yahoo.com>
2019-04-28 23:06:56 +0430

    Make Validation Message Better

--- DigiMovie/Models/Product.cs
+++ DigiMovie/Models/Product.cs
@@ -11,26 +11,26 @@ namespace DigiMovie.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "لطفاً عنوان محصول را وارد نمایید.")]
+        [Required(ErrorMessage = "لطفاً {0} را وارد نمایید.")]
         [StringLength(100, ErrorMessage = "عنوان محصول می بایست حداکثر 100 کاراکتر باشد.")]
         [Display(Name = "عنوان")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "لطفاً مشخصات محصول را وارد نمایید.")]
+        [Required(ErrorMessage = "لطفاً {0} را وارد نمایید.")]
         [Display(Name = "مشخصات")]
         public string Specification { get; set; }
 
         [Display(Name ="موجود")]
         public bool IsExists { get; set; }
 
-        [Required(ErrorMessage = "لطفاً تعداد موجود محصول را وارد نمایید.")]
-        [Range(0,32000,ErrorMessage ="تعداد موجود محصول می بایست حداکثر 32000 باشد.")]
+        [Required(ErrorMessage = "لطفاً {0} را وارد نمایید.")]
+        [Range(0,32000,ErrorMessage = "{0} می بایست حداکثر 32000 باشد.")]
         [Display(Name = "تعداد موجود")]
         public short NumberInStock { get; set; }
 
-        [Required(ErrorMessage = "لطفاً قیمت محصول را وارد نمایید.")]
+        [Required(ErrorMessage = "لطفاً {0} را وارد نمایید.")]
         [Display(Name ="قیمت (ریال)")]
-        [Range(0, 2000000000, ErrorMessage = "قیمت محصول می بایست حداکثر 2000000000 ریال باشد.")]
+        [Range(0, 2000000000, ErrorMessage = "{0} می بایست حداکثر 2000000000 ریال باشد.")]
         public int Price { get; set; }
     }
 }
