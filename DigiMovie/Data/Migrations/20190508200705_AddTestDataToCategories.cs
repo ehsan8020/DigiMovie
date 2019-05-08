f4d7991d2b32c5d58f6c9035f46f091d67f4c46a
Ehsan <ehsan8020@yahoo.com>
2019-05-09 00:52:30 +0430

    Simple Change On Migration (Not Important)

--- DigiMovie/Data/Migrations/20190508200705_AddTestDataToCategories.cs
+++ DigiMovie/Data/Migrations/20190508200705_AddTestDataToCategories.cs
@@ -10,7 +10,7 @@ namespace DigiMovie.Data.Migrations
             INSERT INTO [Categories] ([Title], [Description], [ImagePath]) VALUES (N'دسته بندی نمونه 1', N'توضیحات مربوط به دسته بندی نمونه 1', N'/UserUploads/Categories/201905081057363889.jpg')
             INSERT INTO [Categories] ([Title], [Description], [ImagePath]) VALUES (N'دسته بندی نمونه 2', N'توضیحات مربوط به دسته بندی نمونه 2', N'/UserUploads/Categories/201905081106397205.jpg')
             INSERT INTO [Categories] ([Title], [Description], [ImagePath]) VALUES (N'دسته بندی نمونه 3', N'توضیحات مربوط به دسته بندی نمونه 3', N'/UserUploads/Categories/201905081107032012.jpg')
-            INSERT INTO [Categories] ([Title], [Description], [ImagePath]) VALUES (N'دسته بندی نمونه 4', N'دسته بندی نمونه 4', N'/UserUploads/Categories/201905081107180979.jpg')
+            INSERT INTO [Categories] ([Title], [Description], [ImagePath]) VALUES (N'دسته بندی نمونه 4', N'توضیحات مربوط به دسته بندی نمونه 4', N'/UserUploads/Categories/201905081107180979.jpg')
             INSERT INTO [Categories] ([Title], [Description], [ImagePath]) VALUES (N'دسته بندی نمونه 5', N'توضیحات مربوط به دسته بندی نمونه 5', N'/UserUploads/Categories/201905091234249866.jpg')
             ");
         }
